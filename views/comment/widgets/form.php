26e457421930e153645aa12910fb18a4054c2237
Felix Hahn <info@hahn-felix.de>
2023-04-07 14:25:56 +0200

    Update form.php
    trying to keep the theme specific changes in place (not perfectly)

--- views/comment/widgets/form.php
+++ views/comment/widgets/form.php
@@ -1,59 +1,97 @@
 <?php
 
+use humhub\modules\content\Module;
+use humhub\modules\file\handler\BaseFileHandler;
+use humhub\modules\file\widgets\FileHandlerButtonDropdown;
+use humhub\modules\ui\form\widgets\ActiveForm;
+use humhub\modules\ui\view\components\View;
 use humhub\widgets\Button;
 use yii\helpers\Html;
 use yii\helpers\Url;
 use humhub\modules\content\widgets\richtext\RichTextField;
 use humhub\modules\file\widgets\UploadButton;
 use humhub\modules\file\widgets\FilePreview;
+use humhub\modules\comment\models\Comment;
 
-/* @var $modelName string */
-/* @var $modelId integer */
+/* @var $this View */
+/* @var $objectModel string */
+/* @var $objectId integer */
+/* @var $model Comment */
+/* @var $id string unique object id */
+/* @var $isNestedComment boolean */
+/* @var $contentModule Module */
+/* @var $mentioningUrl string */
+/* @var $isHidden bool */
+/* @var $fileHandlers BaseFileHandler[] */
 
+$contentModule = Yii::$app->getModule('content');
 $submitUrl = Url::to(['/comment/comment/post']);
 
+$placeholder = ($isNestedComment)
+    ? Yii::t('CommentModule.base', 'Write a new reply...')
+    : Yii::t('CommentModule.base', 'Write a new comment...');
 ?>
 
-<div id="comment_create_form_<?= $id; ?>" class="comment_create" data-ui-widget="comment.Form" tabindex="0">
+<div id="comment_create_form_<?= $id ?>" class="comment_create" data-ui-widget="comment.Form"
+     style="<?php if ($isHidden): ?>display:none<?php endif; ?>">
 
-    <?= Html::beginForm('#'); ?>
+    <hr>
+
+    <?php $form = ActiveForm::begin([
+        'action' => $submitUrl,
+        'acknowledge' => true,
+    ]) ?>
+
     <?= Html::hiddenInput('objectModel', $objectModel) ?>
     <?= Html::hiddenInput('objectId', $objectId) ?>
 
     <div class="comment-create-input-group">
-        <?= RichTextField::widget([
+        <?= $form->field($model, 'message')->widget(RichTextField::class, [
             'id' => 'newCommentForm_' . $id,
+            'form' => $form,
             'layout' => RichTextField::LAYOUT_INLINE,
             'pluginOptions' => ['maxHeight' => '300px'],
-            'placeholder' => Yii::t('CommentModule.widgets_views_form', 'Write a new comment...'),
-            'name' => 'message'
-        ]); ?>
-    </div>
-
-    <div id="comment_create_upload_progress_<?= $id ?>" style="display:none;margin-top:10px;margin-right:12px;margin-left:12px; margin-bottom:7px;"></div>
+            'mentioningUrl' => $mentioningUrl,
+            'placeholder' => $placeholder,
+            'events' => [
+                'scroll-active' => 'comment.scrollActive',
+                'scroll-inactive' => 'comment.scrollInactive'
+            ]
+        ])->label(false) ?>
+	</div>
+	
+		<div id="comment_create_upload_progress_<?= $id ?>" style="display:none;margin:10px 12px 12px 7px;"></div>
 
     <?= FilePreview::widget([
         'id' => 'comment_create_upload_preview_' . $id,
-        'options' => ['style' => 'margin-top:10px; margin-right:12px; margin-left:12px; margin-bottom:7px;'],
+        'options' => ['style' => 'margin:10px 12px 12px 7px'],
         'edit' => true
-    ]); ?>
-
-    <div class="comment-buttons">
-        <div class="comment-frame">
-            <?= UploadButton::widget([
+    ]) ?>
+		
+        <div class="comment-buttons">
+			<div class="comment-frame"><?php
+            $uploadButton = UploadButton::widget([
                 'id' => 'comment_create_upload_' . $id,
+                'tooltip' => Yii::t('ContentModule.base', 'Attach Files'),
                 'options' => ['class' => 'main_comment_upload'],
                 'progress' => '#comment_create_upload_progress_' . $id,
                 'preview' => '#comment_create_upload_preview_' . $id,
-                'dropZone' => '#comment_create_form_'.$id,
-                'max' => Yii::$app->getModule('content')->maxAttachedFiles
-            ]); ?>
-
-            <?= Button::defaultType(Yii::t('CommentModule.widgets_views_form', 'Send'))
-                ->cssClass('btn-comment-submit')
-                ->action('submit', $submitUrl)->submit()->sm() ?>
-        </div>
+                'dropZone' => '#comment_create_form_' . $id,
+                'max' => $contentModule->maxAttachedFiles,
+                'cssButtonClass' => 'btn-sm btn-info',
+            ]);
+            echo FileHandlerButtonDropdown::widget([
+                'primaryButton' => $uploadButton,
+                'handlers' => $fileHandlers,
+                'cssButtonClass' => 'btn-info btn-sm',
+                'pullRight' => true,
+            ]);
+            echo Button::defaultType()
+                ->icon('send')
+                ->cssClass('btn-comment-submit')->sm()
+                ->action('submit', $submitUrl)->submit();
+        ?></div>
     </div>
 
-    <?= Html::endForm(); ?>
+    <?php ActiveForm::end() ?>
 </div>
